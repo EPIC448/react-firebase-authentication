a999ac07388b11f81d30dd567685ad9f91bb7789
EPIC448 <samrey2018@gmail.com>
2021-05-09 18:06:37 -0400

    created routers in components/App/index.js file. An import the needed components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
+
+
 import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+
+import * as ROUTES from '../../constants/routes';
+
+
 
 const App = () => (
     <Router>
+        <div>
         <Navigation/>
+            <hr />
+            <Router exact path = {ROUTES.LANDING} component ={LandingPage} />
+            <Router exact path = {ROUTES.SIGN_UP} component ={SignUpPage} />
+            <Router exact path = {ROUTES.SIGN_IN} component ={SignInPage} />
+            <Router exact path = {ROUTES.PASSWORD_FORGET} component ={PasswordForgetPage} />
+            <Router exact path = {ROUTES.HOME} component ={HomePage} />
+            <Router exact path = {ROUTES.ACCOUNT} component ={AccountPage} />
+            <Router exact path = {ROUTES.ADMIN} component ={AdminPage} />
+
+        </div>
     </Router>
 )
 export default App;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
@@ -7,19 +7,19 @@ const Navigation = () => (
     <div>
         <ul>
             <li>
-                <Link to ={ROUTES.SIGN_IN}>Sign In</Link>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
             </li>
             <li>
-                <Link to ={ROUTES.LANDING}>Landing</Link>
+                <Link to={ROUTES.LANDING}>Landing</Link>
             </li>
             <li>
-                <Link to ={ROUTES.HOME}>Home</Link>
+                <Link to={ROUTES.HOME}>Home</Link>
             </li>
             <li>
-                <Link to ={ROUTES.ACCOUNT}>Account</Link>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
             </li>
             <li>
-                <Link to ={ROUTES.ADMIN}>Admin</Link>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
             </li>
         </ul>
     </div>
