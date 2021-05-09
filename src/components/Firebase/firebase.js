a7a03cc9fb3753e02bd805c00972707276351e0d
EPIC448 <samrey2018@gmail.com>
2021-05-09 18:28:28 -0400

    installed Firebase and update it with needed authorization from Firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+
+ const config = {
+    apiKey: "AIzaSyBMV1oqBv5FwqxS0rmLaen9K0KtZIgti7A",
+    authDomain: "react-firebase-authentic-96898.firebaseapp.com",
+    projectId: "react-firebase-authentic-96898",
+    storageBucket: "react-firebase-authentic-96898.appspot.com",
+    messagingSenderId: "238049515325",
+    appId: "1:238049515325:web:e362877b2eab45ae1a5754",
+    measurementId: "G-VCVXP6SV14"
+  };
