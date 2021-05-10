5bbf92d02c322f234440eb4ccfcbfe7a3962aec0
EPIC448 <samrey2018@gmail.com>
2021-05-09 22:22:44 -0400

    And Auth and connected to Firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,6 +15,21 @@ const config = {
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth()
     }
+
+    //*** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    //***doSignInWithEmailAndPassword */
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    
+    doSignOut = () => this.auth.signOut()
+    
+    doPasswordRest = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
 }
 export default Firebase;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 ReactDOM.render(
   <React.StrictMode>
+    <FirebaseContext.Provider value={new Firebase()}>
     <App />
+    </FirebaseContext.Provider>,
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
