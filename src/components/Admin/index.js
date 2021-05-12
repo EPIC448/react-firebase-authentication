de716c3b01a74a9d348d0bbb9d0469d6e7e7dc64
EPIC448 <samrey2018@gmail.com>
2021-05-12 11:24:45 -0400

    updated the Admin section

---
+++ src/components/Admin/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import { withFirebase } from ' ../Firebase';
+
+class AdminPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            users: {},
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ loding: true });
+        
+        this.props.firebase.users().on('value', snapshot => {
+            this.setState({
+                users: snapshot.val(),
+                loading: false,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Admin</h1>
+            </div>
+        )
+    }
+}
+export default withFirebase(AdminPage);
